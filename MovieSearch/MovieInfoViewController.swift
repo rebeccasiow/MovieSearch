7fd884b2b8573d1ddbe0dbd00f87762181ffbc20
rebeccasiow <rebecca.siow@gmail.com>
2016-10-21 23:49:50 -0500

    ratings

--- MovieSearch/MovieInfoViewController.swift
+++ MovieSearch/MovieInfoViewController.swift
@@ -26,12 +26,61 @@ class MovieInfoViewController: UIViewController {
     @IBOutlet weak var imdb: UILabel!
     @IBOutlet weak var likeButton: UIButton!
     
+    @IBOutlet weak var like1: UIButton!
+    @IBOutlet weak var like2: UIButton!
+    @IBOutlet weak var like3: UIButton!
+    @IBOutlet weak var like4: UIButton!
+    @IBOutlet weak var like5: UIButton!
     //Add Movie Title to the faveList global array if not aleady in it.
     //Remove from favourites if not liked anymore
+
+    
+    @IBAction func ratingsChanged(sender: UIButton) {
+        
+        switch sender {
+        case like1:
+            like1.highlighted = true
+            like2.highlighted = false
+            like3.highlighted = false
+            like4.highlighted = false
+            like5.highlighted = false
+            return
+        case like2:
+            like1.highlighted = true
+            like2.highlighted = true
+            like2.highlighted = false
+            like3.highlighted = false
+            like4.highlighted = false
+            like5.highlighted = false
+            return
+        case like3:
+            like1.highlighted = true
+            like2.highlighted = true
+            like3.highlighted = true
+            like4.highlighted = false
+            like5.highlighted = false
+            return
+        case like4:
+            like1.highlighted = true
+            like2.highlighted = true
+            like3.highlighted = true
+            like4.highlighted = true
+            like5.highlighted = false
+            return
+        case like5:
+            like1.highlighted = true
+            like2.highlighted = true
+            like3.highlighted = true
+            like4.highlighted = true
+            like5.highlighted = true
+            return
+        default:
+            return
+        }
+        
+    }
     @IBAction func addToFavourites(sender: UIButton) {
         print("add this movie")
-        
-        //likeButton.imageView?.image = likeButton.imageView?.highlightedImage
         
         movieLiked = !movieLiked
         
@@ -56,6 +105,14 @@ class MovieInfoViewController: UIViewController {
         imdb.text = movieInfo.imdb
         movieLiked = false
         // Do any additional setup after loading the view.
+
+        like1.setImage(UIImage(named:"starred.png"), forState: .Highlighted)
+        like2.setImage(UIImage(named:"starred.png"), forState: .Highlighted)
+        like3.setImage(UIImage(named:"starred.png"), forState: .Highlighted)
+        like4.setImage(UIImage(named:"starred.png"), forState: .Highlighted)
+        like5.setImage(UIImage(named:"starred.png"), forState: .Highlighted)
+                like1.setImage(UIImage(named:"starred.png"), forState: .Highlighted)
+        
     }
 
     override func didReceiveMemoryWarning() {
